eeb08bcac6b8e87c3d264b71f6d2a161995052b7
Einar Löve <post@einarlove.com>
2017-02-27 15:11:50 +0100

    Correctly compile a umd bundle (#28)

--- scripts/build-umd.js
+++ scripts/build-umd.js
@@ -6,12 +6,25 @@ webpack({
   entry: './lib',
   output: {
     filename: './lib/umd.js',
+    libraryTarget: 'umd',
     library: 'reactFirebase',
   },
   externals: {
-    react: 'React',
-    'firebase/database': 'firebase',
-    'firebase/app': 'firebase',
+    react: {
+      root: 'React',
+      commonjs: 'react',
+      commonjs2: 'react',
+    },
+    'firebase/database': {
+      root: 'firebase',
+      commonjs: 'firebase/database',
+      commonjs2: 'firebase/database',
+    },
+    'firebase/app': {
+      root: 'firebase',
+      commonjs: 'firebase/app',
+      commonjs2: 'firebase/app',
+    },
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin(),
