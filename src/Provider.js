b062d6685ad712cbb1715c4118749a6d5a556325
David Ascher <david.ascher@gmail.com>
2017-05-08 04:16:02 -0700

    switch to using prop-types for React 15.5+ compat (#38)
    * switch to using prop-types
    
    * clean lint rules

--- src/Provider.js
+++ src/Provider.js
@@ -1,4 +1,5 @@
-import { Component, PropTypes, Children } from 'react'
+import PropTypes from 'prop-types'
+import { Component, Children } from 'react'
 
 export default class Provider extends Component {
   static propTypes = {
